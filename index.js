bd1e79e9e9831bb55cd32c5602ef71317a18d607
Laura Rinehimer <rinehimerl@alumni.misericordia.edu>
2017-10-31 16:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function shout(string){
   return string.toUpperCase()
 }
+
 function whisper(string){
   return string.toLowerCase()
 }
+
 function logShout(string){
-  console.log('HELLO')
+  console.log(string.toUpperCase())
 }
+
 function logWhisper(string){
-  console.log('hello')
+  console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string){
-  return ('I can\'t hear you')
-  if (string.LowerCase)
-  else ('YES INDEED')
+  if (string.toUpperCase()===string){
+    return "YES INDEED!"
+  }
+  else if (string.toLowerCase()===string){
+    return "I can't hear you!"
+  }
+  else if (string==="I love you Grandma"){
+    return "I love you, too."
+  }
 }
